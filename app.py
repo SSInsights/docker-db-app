d868383ede00da0431b1ceb0bb09ee0882b80781
SSInsights <shivpal.aiml50@gmail.com>
2023-10-11 19:47:26 +0530

    api funcion  added for taking user record in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask,request
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import os
@@ -11,29 +11,29 @@ password = os.environ['POSTGRES_PASSWORD']
 def hello_world():
     return 'Hello, Docker!'
 
-@app.route('/widgets')
-def get_widgets():
-    conn = psycopg2.connect(
-        host="db",
-        user="postgres",
-        password=password,
-        database="example"
-    )
-    cursor = conn.cursor()
+# @app.route('/widgets')
+# def get_widgets():
+#     conn = psycopg2.connect(
+#         host="db",
+#         user="postgres",
+#         password=password,
+#         database="example"
+#     )
+#     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM widgets")
+#     cursor.execute("SELECT * FROM widgets")
 
-    row_headers=[x[0] for x in cursor.description]
+#     row_headers=[x[0] for x in cursor.description]
 
-    results = cursor.fetchall()
-    json_data=[]
-    for result in results:
-        json_data.append(dict(zip(row_headers,result)))
+#     results = cursor.fetchall()
+#     json_data=[]
+#     for result in results:
+#         json_data.append(dict(zip(row_headers,result)))
 
-    cursor.close()
-    conn.close()
+#     cursor.close()
+#     conn.close()
 
-    return json.dumps(json_data)
+#     return json.dumps(json_data)
 
 @app.route('/initdb')
 def db_init():
@@ -62,13 +62,65 @@ def db_init():
     cursor = conn.cursor()
 
     cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+    cursor.execute("CREATE TABLE example (name VARCHAR(255), description VARCHAR(255))")
 
     cursor.close()
     conn.commit()
     conn.close()
 
     return 'init database'
+
+
+
+# New route to insert a single record into the "example" table
+@app.route('/add_record', methods=['POST'])
+def add_record():
+    data = request.get_json()
+    if 'name' not in data or 'description' not in data:
+        return 'Invalid data format', 400
+
+    conn = psycopg2.connect(
+        host="db",
+        user="postgres",
+        password=password,
+        database="example"
+    )
+    cursor = conn.cursor()
+
+    insert_query = "INSERT INTO example (name, description) VALUES (%s, %s)"
+    cursor.execute(insert_query, (data['name'], data['description']))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return 'Record added to the "example" table successfully', 201
+
+
+
+# # Modify the existing route to insert a single record into the "example" table
+# @app.route('/widgets', methods=['POST'])
+# def add_widget():
+#     data = request.get_json()
+#     if 'name' not in data or 'description' not in data:
+#         return 'Invalid data format', 400
+
+#     conn = psycopg2.connect(
+#         host="docker-db-app-db-1",
+#         user="postgres",
+#         password=password,
+#         database="example"
+#     )
+#     cursor = conn.cursor()
+
+#     insert_query = "INSERT INTO example (name, description) VALUES (%s, %s)"
+#     cursor.execute(insert_query, (data['name'], data['description']))
+
+#     conn.commit()
+#     cursor.close()
+#     conn.close()
+
+#     return 'Record added to the "example" table successfully', 201
 
 
 # CREATE ONE ENDPOINT to insert one record in table 
